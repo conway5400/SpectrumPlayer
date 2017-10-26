8d5d3d83d712118fc0cc96145de8e0f460c63ccb
Conway Solomon <conwaysolomon@Conways-MacBook-Pro.local>
2017-10-26 01:26:41 -0700

    initial commit

---
+++ apps/WebPlayer/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'WebPlayer/index.html')
+
+def alexa(request):
+    print 'woooohhooooo'
+    return HttpResponse('Alexa works!')
