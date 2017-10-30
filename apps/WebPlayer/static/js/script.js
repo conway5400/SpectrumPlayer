6022ed13122c33a436f44a628d3d47f5027e6cc9
Conway Solomon <conwaysolomon@Conways-MacBook-Pro.local>
2017-10-30 00:04:19 -0700

    updates

--- apps/WebPlayer/static/js/script.js
+++ apps/WebPlayer/static/js/script.js
@@ -4,13 +4,11 @@ var url = "https://dash.edgesuite.net/envivio/Envivio-dash2/manifest.mpd";
 var element = document.querySelector("#player")
 var player = dashjs.MediaPlayer().create();
 var checkForUpdates = true;
-
 player.initialize(element, url, false);
 
 
 $('#play').on('click', function() {
     player.play();
-    checkForUpdates = true;
     checkUpdates();
 });
 
@@ -22,15 +20,23 @@ function checkUpdates() {
     if(checkForUpdates) {
         console.log('checking for updates')
 
-        $.get( "/updates", function( command ) {
-
-            if(command == 'play') {
-                player.play();
-            } else if (command == 'pause') {
-                player.pause();
+        try {
+                $.get( "/updates", function( command ) {
+                    console.log(command)
+                    if(command == 'play') {
+                        player.play();
+                    } else if (command == 'pause') {
+                        player.pause();
+                    }
+                    $('#connectionStatus').html('Connected!');
+                    setTimeout(checkUpdates, 1000)
+                });
+            } catch (error) {
+                checkForUpdates = false;
+                $('#connectionStatus').html('Error connecting!');
+                console.error('there was an error! abandoning ship!')
             }
-
-            setTimeout(checkUpdates, 1000)
-        });
     }
 }
+
+checkUpdates();
